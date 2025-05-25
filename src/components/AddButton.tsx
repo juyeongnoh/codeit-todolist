89136cd286adb0f668e6e9d0f3e3c659044e63c0
Juyeong "James" Noh <juyeongnoh@gmail.com>
2025-05-26 01:38:21 +0900

    style: 추가 버튼 스타일 추가

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -4,10 +4,13 @@ export default function AddButton({
   return (
     <button
       type="button"
-      className="bg-slate-200 rounded-3xl sm:w-40 w-14 h-14 py-4 flex gap-1 items-center justify-center shadow-[4px_3.5px_0px_0_#0F172A] shrink-0 border-slate-900 border-2 cursor-pointer"
+      className="disabled:bg-slate-200 bg-violet-600 enabled:text-white rounded-3xl sm:w-40 w-14 h-14 py-4 flex gap-1 items-center justify-center shadow-[4px_3.5px_0px_0_#0F172A] shrink-0 border-slate-900 border-2 cursor-pointer"
       {...props}
     >
-      <img src="/ic/plus_black.svg" />
+      <img
+        src="/ic/plus_black.svg"
+        className={props.disabled ? "" : "invert"}
+      />
       <div className="hidden sm:block">추가하기</div>
     </button>
   );
